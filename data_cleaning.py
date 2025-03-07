f6781ca1768df22c288740ed0180b71c2ee2cf78
Sara <39550635+pullz6@users.noreply.github.com>
2025-03-07 23:49:19 +0000

    Add files via upload

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,3 +3,7 @@ import pandas as pd
 
 df = loading_df()
 print(df.info())
+df["date"] = pd.to_datetime(df["date"])
+df["time"] = pd.to_datetime(df["time"], format="%H:%M:%S:%f")
+df["time"] = df["time"].dt.time
+print(df.info())
