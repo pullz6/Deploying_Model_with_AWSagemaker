911c30db94f125ece5660b29cd96f030411d4c6b
Sara <39550635+pullz6@users.noreply.github.com>
2025-03-10 23:16:19 +0000

    Add files via upload

--- run_training.py
+++ run_training.py
@@ -6,22 +6,23 @@ from mlflow.models import infer_signature
 
 #Importing libraries
 import numpy as np
-from sklearn.model_selection import train_test_split
 import pandas as pd
-from data_cleaning import * 
-from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error 
+
+def split_data(): 
+    """This function is used to split the data"""
+    df = cleaning_df()
+    X = df.drop(['wrist','time','date','gyro_x','gyro_y','gyro_z'],axis=1)
+    y = df['activity']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101) 
+    return  X_train, X_test, y_train, y_test
 
 print('starting the run')
     #mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
     # Create a new MLflow Experiment
 mlflow.set_experiment("Kinematics to see phone activity")
-
-df = cleaning_df()
-X = df.drop(['wrist','time','date','gyro_x','gyro_y','gyro_z'],axis=1)
-y = df['activity']
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101) 
+X_train, X_test, y_train, y_test = split_data()
 model = model()
 model.fit(X_train,y_train)
 predictions = model.predict(X_test)
