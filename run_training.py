194b2fd6f0793e9fc21491aabb916765d606456f
Sara <39550635+pullz6@users.noreply.github.com>
2025-03-10 22:33:42 +0000

    Add files via upload
    connected with mlflow.

--- run_training.py
+++ run_training.py
@@ -2,6 +2,7 @@
 from data_cleaning import * 
 from model import * 
 import mlflow
+from mlflow.models import infer_signature
 
 #Importing libraries
 import numpy as np
@@ -11,6 +12,11 @@ from data_cleaning import *
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error 
 
+print('starting the run')
+    #mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
+    # Create a new MLflow Experiment
+mlflow.set_experiment("Kinematics to see phone activity")
+
 df = cleaning_df()
 X = df.drop(['wrist','time','date','gyro_x','gyro_y','gyro_z'],axis=1)
 y = df['activity']
@@ -19,5 +25,30 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 model = model()
 model.fit(X_train,y_train)
 predictions = model.predict(X_test)
+rmse = mean_squared_error(y_test, predictions)
+mse = mean_absolute_error(y_test, predictions)
 print('mean_squared_error : ', mean_squared_error(y_test, predictions)) 
 print('mean_absolute_error : ', mean_absolute_error(y_test, predictions))
+
+with mlflow.start_run():
+    # Log the hyperparameters
+        #mlflow.log_params(params)
+
+        # Log the loss metric
+        mlflow.log_metric("RMSE", rmse)
+        mlflow.log_metric("MSE", mse)
+
+        # Set a tag that we can use to remind ourselves what this run was for
+        mlflow.set_tag("Training Info", "Training a linear regression model to predict kinematic movement through phones")
+
+        # Infer the model signature
+        signature = infer_signature(X_train, model.predict(X_train))
+
+        # Log the model
+        model_info = mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="Kinematic model",
+            signature=signature,
+            input_example=X_train,
+            registered_model_name="Model.v.0.1",
+        ) 
