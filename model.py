3f0072aa4ac5eff07193aa1005d04ca0c49ac3b9
Sara <39550635+pullz6@users.noreply.github.com>
2025-03-11 23:03:45 +0000

    Add files via upload
    creating model pipeline.

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 import pandas as pd 
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error 
+from sklearn.pipeline import make_pipeline
 
 def model(): 
     model = LinearRegression()
