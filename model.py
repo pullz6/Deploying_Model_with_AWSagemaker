13e9587ade38335cffa0ed650104721a2b1a3990
Sara <39550635+pullz6@users.noreply.github.com>
2025-03-12 22:11:43 +0000

    Add files via upload

--- model.py
+++ model.py
@@ -1,17 +1,44 @@
 #Importing Libraries
 import numpy as np
 import pandas as pd 
+from numpy import mean, std
 
 #Utility for sklearn 
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import RepeatedKFold, cross_val_score
 
 #Importing the models from Sklearn
 import xgboost as xgb
 from sklearn.linear_model import LinearRegression
+from sklearn.naive_bayes import GaussianNB
 
-def model(): 
-    lr_model = LinearRegression()
-    xgb_model = xgb.XGBRegressor(objective="reg:linear", random_state=42)
-    model = make_pipeline(lr_model, xgb_model)
-    return model
+
+# get a stacking ensemble of models
+def get_stacking():
+	# define the base models
+	level0 = list()
+	level0.append(('lr', LinearRegression()))
+	level0.append(('bayes', GaussianNB()))
+    level0.append(('xgb',xg.XGBRegressor(objective='reg:squarederror', n_estimators=10, seed=123)))
+	# define meta learner model
+	level1 = LinearRegression()
+	# define the stacking ensemble
+	model = StackingClassifier(estimators=level0, final_estimator=level1, cv=5)
+	return model
+ 
+# get a list of models to evaluate
+def get_models():
+	models = dict()
+	models['lr'] = LinearRegression()
+	models['bayes'] = GaussianNB()
+	models['stacking'] = get_stacking()
+    models['xgb'] = xg.XGBRegressor(objective='reg:squarederror', n_estimators=10, seed=123)
+	return models
+ 
+def evaluate_model(model, X, y):
+    # Define cross-validation method (Repeated K-Fold for regression)
+    cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+    # Evaluate model using cross-validation with negative RMSE scoring
+    scores = cross_val_score(model, X, y, scoring='neg_root_mean_squared_error', cv=cv, n_jobs=-1, error_score='raise')
+    return scores
+ 
