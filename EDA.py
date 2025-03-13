2f8fa0c6eed548fe60cd0b914c4960f9a79947d7
Sara <39550635+pullz6@users.noreply.github.com>
2025-03-13 22:37:25 +0000

    Delete EDA.py

--- EDA.py
+++
@@ -1,7 +0,0 @@
-#Importing our classes and their functions
-from data_ingestion.py import * 
-
-#Import libraries required
-import mlflow.data
-import pandas as pd
-from mlflow.data.pandas_dataset import PandasDataset
