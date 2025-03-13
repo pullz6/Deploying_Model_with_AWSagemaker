120b0fd69485e3577d9337b408440bdf86f048b0
Sara <39550635+pullz6@users.noreply.github.com>
2025-03-13 22:37:50 +0000

    Add files via upload

---
+++ log_df.py
@@ -0,0 +1,7 @@
+#Importing our classes and their functions
+from data_ingestion.py import * 
+
+#Import libraries required
+import mlflow.data
+import pandas as pd
+from mlflow.data.pandas_dataset import PandasDataset
