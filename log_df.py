aef802fd10f52f57d2d7e42081c2334775c39f58
Sara <39550635+pullz6@users.noreply.github.com>
2025-03-14 14:18:49 +0000

    Add files via upload
    Added into mlflow, need to add to a specific experimentation.

--- log_df.py
+++ log_df.py
@@ -5,11 +5,18 @@ from data_ingestion import *
 import mlflow.data
 import pandas as pd
 from mlflow.data.pandas_dataset import PandasDataset
+from mlflow.data.sources import LocalArtifactDatasetSource
 
 
 dataset_source_url = "https://www.kaggle.com/datasets/yasserh/kinematics-motion-data/data"
+
 raw_data = loading_df()
 
 dataset = mlflow.data.from_pandas(
     raw_data, source=dataset_source_url, name="Kinematics to see phone activity", targets="activity"
 )
+
+with mlflow.start_run():
+    # Log the dataset to the MLflow Run. Specify the "training" context to indicate that the
+    # dataset is used for model training
+    mlflow.log_input(dataset, context="training")
